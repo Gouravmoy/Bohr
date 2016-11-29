50c9b48165514e65ea41c74e8ad0ffaf25f21bdc
Gouravmoy <gouravmoy.mohanty91@gmail.com>
2016-11-29 13:48:44 +0530

    Prop View Added

---
+++ DataGenerator/src/views/util/MainUtil.java
@@ -0,0 +1,25 @@
+package views.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.Columnsdetail;
+
+public class MainUtil {
+
+	List<String> returnList;
+
+	public List<String> getColumnsdetail(Columnsdetail col) {
+		returnList = new ArrayList<>();
+		returnList.add("Name:" + col.getName());
+		returnList.add("Type:" + col.getType());
+		returnList.add("Length:" + col.getLength());
+		returnList.add("Decimal Length:" + col.getDecimalLength());
+		returnList.add("Is Nullable:" + (col.getIsnullable() == 1 ? "True" : "False"));
+		returnList.add("Key Type:" + col.getKeytype());
+		if (col.getEnumvalues() != null)
+			returnList.add("Column Enum Values:" + col.getEnumvalues());
+		return returnList;
+	}
+
+}
